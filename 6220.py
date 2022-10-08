899bafb21944223adbb94c9567de0ed2b0865599
sulabook <sula.book@gmail.com>
2022-10-08 19:49:18 +0900

    6220

---
+++ 6220.py
@@ -0,0 +1,12 @@
+c = input()
+if c.islower():
+    print(f'{c}는 소문자 입니다.')
+else:
+    print(f'{c}는 대문자 입니다.')
+
+'''
+if ord('a') <= ord('c') <= ord('z'):
+    print(~)
+elif ord('A') <= ord('c') <= ord('Z'):
+    print(~)
+'''
