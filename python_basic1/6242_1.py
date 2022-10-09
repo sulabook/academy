e1aed858e64b7179c7c453c01ae9e98065ad1087
sulabook <sula.book@gmail.com>
2022-10-09 22:31:15 +0900

    6242

---
+++ python_basic1/6242_1.py
@@ -0,0 +1,7 @@
+bloodTypes = ['A', 'A', 'A', 'O', 'B', 'B', 'O', 'AB', 'AB', 'O']
+typeCounter = {'A':0, 'O':0, 'B':0, 'AB':0}
+
+for bloodType in bloodTypes:
+    typeCounter[bloodType] += 1
+
+print(f"{{'A': {typeCounter['A']}, 'O': {typeCounter['O']}, 'B': {typeCounter['B']}, 'AB': {typeCounter['AB']}}}")
