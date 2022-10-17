19f6b31fa0a53dc9d4ffd1f6606211b8bb511b6c
sulabook <sula.book@gmail.com>
2022-10-17 19:46:09 +0900

    6276

---
+++ python_basic2/6276.py
@@ -0,0 +1,11 @@
+result = [0] * 9
+
+for i in range(2, 10):
+    result[i - 2]  = list()
+    for j in range(1, 10):
+        if i * j % 3 == 0 or i * j % 7 == 0:
+            continue
+        else:
+            result[i - 2].append(i * j)
+
+print(result)
