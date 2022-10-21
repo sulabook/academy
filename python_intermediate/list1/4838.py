6c117443345d8b1a671bb7b177e777ca52581069
sulabook <sula.book@gmail.com>
2022-10-21 20:03:05 +0900

    4838

--- python_intermediate/list1/4838.py
+++ python_intermediate/list1/4838.py
@@ -2,6 +2,6 @@ T = int(input())
 data = list()
 for i in range(T):
     N = input()
-    data.append(input().split(' '))
+    data.append(list(map(int, input().split(' '))))
 for i in range(T):
-    print(f'#{i + 1} {int(max(data[i])) - int(min(data[i]))}')
+    print(f'#{i + 1} {max(data[i]) - min(data[i])}')
