038a87bf09ceebc6f070089a2e159679a0005835
sulabook <sula.book@gmail.com>
2022-10-28 17:00:31 +0900

    4831

--- python_intermediate/list1/4831.py
+++ python_intermediate/list1/4831.py
@@ -18,12 +18,13 @@ for t in range(T):
     for i in range(2 ** M):     # 각각의 경우에서
         for j in range(1, M + 1):       # 충전기 위치에 0 / 1 분기를 만든다
             if (i // (2 ** (M - j))) % 2 == 0:      
-                routes[i][chargingStations[j - 1]] = 0
-            else:
                 routes[i][chargingStations[j - 1]] = 1
+            else:
+                routes[i][chargingStations[j - 1]] = 0
 
     # 모든 경우를 순회하며 최소값을 찾음
     min_charge = M + 1      # 충전기수보다 1 큼. 종점 도착하는 경우 무조건 바뀜
+    ox = False
     for route in routes:
         k = K       # 충전량
         i = 0       # 위치
@@ -33,11 +34,15 @@ for t in range(T):
             if i == N:      # 종점 도착 시
                 if min_charge > sum(route):     # 값이 작으면 저장
                     min_charge = sum(route)
+                    ox = True
             k -= 1      # 충전량 1 감소
             if route[i] == 1:       # 충전기 있는 정류장이면 충전
                 k = K
+        
+        if not ox:
+            break
 
-    if min_charge == M + 1:     # 도착하지 못하는 경우
+    if not ox:     # 도착하지 못하는 경우
         print(f'#{t + 1} 0')        # 0 출력
     else:
         print(f'#{t + 1} {min_charge}')
