c07ca0cf68221aab2465d249be17cf977707aea5
sulabook <sula.book@gmail.com>
2022-10-28 17:26:00 +0900

    4831 아 안된당 ㅠ

--- python_intermediate/list1/4831.py
+++ python_intermediate/list1/4831.py
@@ -6,33 +6,32 @@ for t in range(T):
     # M: 충전기가 있는 정류장 수
     K, N, M = map(int, input().split(' '))
 
-    # chargingStations: 충전기가 있는 정류장 위치
-    chargingStations = list(map(int, input().split(' ')))
+    # C: 충전기가 있는 정류장 위치
+    C = list(map(int, input().split(' ')))
 
     # 모든 경우의 수
-    routes = [[0] * (N + 1) for _ in range(2 ** M)]
+    R = [[0] * (N + 1) for _ in range(2 ** M)]
 
     for i in range(2 ** M):     # 각각의 경우에서
         for j in range(1, M + 1):       # 충전기 위치에 0 / 1 분기를 만든다
             if (i // (2 ** (M - j))) % 2 == 0:      
-                routes[i][chargingStations[j - 1]] = 1
+                R[i][C[j - 1]] = 1
             else:
-                routes[i][chargingStations[j - 1]] = 0
-    print(routes)
+                R[i][C[j - 1]] = 0
 
     # 모든 경우를 순회하며 최소값을 찾음
     min_charge = M + 1      # 충전기수보다 1 큼. 종점 도착하는 경우 무조건 바뀜
-    for route in routes:
+    for r in R:
         k = K       # 충전량
         i = 0       # 위치
 
         while k > 0 and i < N:      
             i += 1      # 위치 1 증가
             if i == N:      # 종점 도착 시
-                if min_charge > sum(route):     # 값이 작으면 저장
-                    min_charge = sum(route)
+                if min_charge > sum(R):     # 값이 작으면 저장
+                    min_charge = sum(R)
             k -= 1      # 충전량 1 감소
-            if route[i] == 1:       # 충전기 있는 정류장이면 충전
+            if R[i] == 1:       # 충전기 있는 정류장이면 충전
                 k = K
         
         if min_charge == M + 1:
