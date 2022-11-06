36429a670de76d991abd5f3fdec85df3d5aba69c
sulabook <sula.book@gmail.com>
2022-11-06 20:59:31 +0900

    4835

--- python_intermediate/list1/4835.py
+++ python_intermediate/list1/4835.py
@@ -3,10 +3,16 @@ T = int(input())
 for t in range(T):
     N, M = map(int, input().split(' '))
     data_list = list(map(int, input().split(' ')))
-    sum_list = [0] * (N - M + 1)
+    min_sum = 1e8
+    max_sum = 1
     for i in range(N - M + 1):
+        sumM = 0
         for j in range(M):
-            sum_list[i] += data_list[i + j]
-    min_sum = min(sum_list)
-    max_sum = max(sum_list)
+            sumM += data_list[i + j]
+        if sumM < min_sum:
+            print('min_sum = %s -> %s' % (min_sum, sumM))
+            min_sum = sumM
+        if sumM > max_sum:
+            print('max_sum = %s -> %s' % (max_sum, sumM))
+            max_sum = sumM
     print(f'#{t + 1} {max_sum - min_sum}')
